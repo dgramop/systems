dc5a6ba4db772a31044663189a5882a5a24c381e
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2026-02-18 15:48:51 -0800

    combine

--- flake.nix
+++ flake.nix
@@ -3,24 +3,32 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, nixpkgs, flake-utils, home-manager, ...}: flake-utils.lib.eachDefaultSystem (system: let
+  outputs = {self, nixpkgs, nixpkgs-unstable, flake-utils, home-manager, ...}: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [(final: prev: {
+        gnuradio = pkgs-unstable.gnuradio;
+      })];
+    };
+
+    pkgs-unstable = import nixpkgs-unstable {
       inherit system;
       config.allowUnfree = true;
     };
   in {
-    homeConfigurations."mac" = home-manager.lib.homeManagerConfiguration {
+    packages.homeConfigurations."mac" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
       modules = [ ./home/mac.nix ];
     };
 
-    homeConfigurations."specter" = home-manager.lib.homeManagerConfiguration {
+    packages.homeConfigurations."specter" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
       modules = [ ./home/specter.nix ];
     };
