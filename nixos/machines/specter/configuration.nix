78ba56bd3320aa88b5dce5a543f3e73f124437dd
Dhruv Gramopadhye <dhruv@specter.co>
2026-02-18 15:02:37 -0800

    docker

--- nixos/machines/specter/configuration.nix
+++ nixos/machines/specter/configuration.nix
@@ -75,17 +75,7 @@
     firefox
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  virtualization.docker.enable = true;
   services.openssh.enable = true;
 
   # This value determines the NixOS release from which the default
